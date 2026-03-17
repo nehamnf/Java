c77685f2f684a2b5a57c57a7771a7d9e9a688ebb
NehaMN <nehamn207@gmail.com>
2026-03-17 21:38:52 +0530

    Generic Class and Generic Method Example

--- JavaConcepts/JavaRecap/src/com/neha/generics/GenericsRecapExample.java
+++ JavaConcepts/JavaRecap/src/com/neha/generics/GenericsRecapExample.java
@@ -3,6 +3,32 @@ package com.neha.generics;
 import java.util.ArrayList;
 import java.util.List;
 
+class Pair<K,V>{
+    private K key;
+    private V value;
+
+    public Pair(K key,V value) {
+        this.value = value;
+        this.key = key;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
+
 //Generic Class Example
 public class GenericsRecapExample<T> {
     private List<T> names = new ArrayList<>();
@@ -20,12 +46,21 @@ public class GenericsRecapExample<T> {
         return choice ? a:b;
     }
 
+    public <K,V> String pairToString(K k,V v){
+        return k+" "+v;
+    }
+
     public static void main(String[] args) {
         GenericsRecapExample<String> list =  new GenericsRecapExample<>();
         list.addNames("neha");
         list.addNames("zohan");
         list.printDetails();
         String choice = list.<String>pickFirst("neha","zohan",false);
+
+        Pair<String, Integer> pair= new Pair<>("neha",100);
+        System.out.println(pair.getKey());
+
+        System.out.println(list.pairToString("neha",20));
     }
 }
 
