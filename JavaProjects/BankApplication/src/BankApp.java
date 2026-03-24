4bae04c821635faa6b85a9f45cd96914e8aa069f
nehamn <31812636+nehamnf@users.noreply.github.com>
2026-03-25 01:02:41 +0530

    Delete JavaProjects/BankApplication directory

--- JavaProjects/BankApplication/src/BankApp.java
+++
@@ -1,9 +0,0 @@
-public class BankApp {
-
-    public static void main(String[] args) {
-        Account neha= new Checking("neha","123499",1000);
-        Account safi= new Savings("safi","123499",1000);
-        neha.showInfo();
-        safi.showInfo();
-    }
-}
